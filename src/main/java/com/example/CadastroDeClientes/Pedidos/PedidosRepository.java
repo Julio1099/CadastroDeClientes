8195e713c9ca45cb9870fdd99bd06f3af4ed03ed
Julio1099 <juliio1520@gmail.com>
2026-03-03 01:24:57 -0300

    feat: cria interface PedidosRepository para persistencia de dados

---
+++ src/main/java/com/example/CadastroDeClientes/Pedidos/PedidosRepository.java
@@ -0,0 +1,6 @@
+package com.example.CadastroDeClientes.Pedidos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PedidosRepository extends JpaRepository<PedidosModel, Long> {
+}
