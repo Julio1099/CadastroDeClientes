9b24db7491f1d735e2ff3cd5a7f109244546e567
Julio1099 <juliio1520@gmail.com>
2026-03-04 01:03:13 -0300

    feat: implementa rotas do controller com dados mockados

--- src/main/java/com/example/CadastroDeClientes/Pedidos/PedidosController.java
+++ src/main/java/com/example/CadastroDeClientes/Pedidos/PedidosController.java
@@ -1,4 +1,29 @@
 package com.example.CadastroDeClientes.Pedidos;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("pedidos")
 public class PedidosController {
+
+    @GetMapping("/listar")
+    public String listarPedido(){
+        return "Pedido listada com sucesso";
+    }
+
+    @PostMapping("/criar")
+    public String criarPedido(){
+        return "Pedido criado com sucesso";
+    }
+
+    @PutMapping("/alterar")
+    public String alterarPedido(){
+        return "Pedido alterado com sucesso";
+    }
+
+    @DeleteMapping("/deletar")
+    public String deletarPedido(){
+        return "Pedido deletado com sucesso";
+    }
+
 }
